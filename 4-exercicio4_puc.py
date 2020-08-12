d7a4d491fec61fc7e746581d43d1a3fbfa5a7144
Julio dos Reis Firmino <67196397+julioreis-dev@users.noreply.github.com>
2020-08-12 00:16:42 -0300

    Add files via upload

---
+++ 4-exercicio4_puc.py
@@ -0,0 +1,14 @@
+def vertical():
+    numero = 1
+    while numero < 21:
+        print(numero)
+        numero = numero+1
+
+def horizontal():
+    numero1 = 1
+    while numero1 < 21:
+        print(numero1, end=' ')
+        numero1 = numero1+1
+
+vertical()
+horizontal()
